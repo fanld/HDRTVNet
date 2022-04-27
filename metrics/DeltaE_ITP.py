7ef7f25f55b776b4ca9fa2d7936895a42b0960af
fanld <fanleidong@stu.pku.edu.cn>
2022-04-27 16:04:16 +0800

    add 归一化。

--- metrics/DeltaE_ITP.py
+++ metrics/DeltaE_ITP.py
@@ -6,6 +6,8 @@ import numpy as np
 def calculate_hdr_deltaITP(img1, img2):
     img1 = img1[:, :, [2, 1, 0]]
     img2 = img2[:, :, [2, 1, 0]]
+    img1 = img1/255;
+    img2 = img2/255;
     # 按照BT2100，在计算eotf之前，应该先把输入归一化到[0,1]。所以此代码需要修改。
     img1 = colour.models.eotf_ST2084(img1) #已验证。与BT2100表4的PQ-EOTF计算出来的值完全一致。fanld
     img2 = colour.models.eotf_ST2084(img2)
--- metrics/test_DeltaE_ITP.py
+++ metrics/test_DeltaE_ITP.py
@@ -5,3 +5,5 @@ im1 = cv2.imread('/tmp/HDRTVNET_ubuntu/dataset/dataset/test_set/test_hdr/001.png
 im2 = cv2.imread('/tmp/HDRTVNET_ubuntu/dataset/dataset/test_set/test_hdr/002.png');
 
 out = metrics.DeltaE_ITP.calculate_hdr_deltaITP(im1, im2)
+
+print(out)
